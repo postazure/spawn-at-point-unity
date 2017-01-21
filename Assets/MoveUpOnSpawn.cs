9a2f183b00c1dde0550a6e1c1576b4035801afad
Max Gordon <gordonmaxc@gmail.com>
2017-01-21 07:28:17 -0800

    first commit

---
+++ Assets/MoveUpOnSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUpOnSpawn : MonoBehaviour {
+	public float thrustY = 0;
+	private Rigidbody rbody;
+
+
+	void Start () {
+		rbody = GetComponent<Rigidbody> ();	
+		var target = this.transform.position;
+		target.y = 11;
+
+		rbody.velocity = target - this.transform.position;
+	}
+	
+
+	void Update () {
+
+
+
+		if (this.transform.position.y >= 4) {
+			Destroy (this.gameObject);
+		}
+	}
+}
